36a40b96e9aa3e97925e56e092c3a3cea19a5c3f
Rob Gibbens <RobGibbens@gmail.com>
2013-09-14 22:10:03 -0400

    Step 20

---
+++ MvvmDemo.WinPhone/LocalizedStrings.cs
@@ -0,0 +1,14 @@
+﻿using MvvmDemo.WinPhone.Resources;
+
+namespace MvvmDemo.WinPhone
+{
+	/// <summary>
+	/// Provides access to string resources.
+	/// </summary>
+	public class LocalizedStrings
+	{
+		private static AppResources _localizedResources = new AppResources();
+
+		public AppResources LocalizedResources { get { return _localizedResources; } }
+	}
+}

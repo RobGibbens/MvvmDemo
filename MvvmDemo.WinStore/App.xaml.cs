677daa4238c2c2cbfc913627dfbb5a497bdc0331
Rob Gibbens <RobGibbens@gmail.com>
2013-09-14 22:33:37 -0400

    Step 39

--- MvvmDemo.WinStore/App.xaml.cs
+++ MvvmDemo.WinStore/App.xaml.cs
@@ -39,39 +39,41 @@ namespace MvvmDemo.WinStore
         /// search results, and so forth.
         /// </summary>
         /// <param name="args">Details about the launch request and process.</param>
-        protected override void OnLaunched(LaunchActivatedEventArgs args)
-        {
-            Frame rootFrame = Window.Current.Content as Frame;
+				protected override void OnLaunched(LaunchActivatedEventArgs args)
+				{
+					var rootFrame = Window.Current.Content as Frame;
 
-            // Do not repeat app initialization when the Window already has content,
-            // just ensure that the window is active
-            if (rootFrame == null)
-            {
-                // Create a Frame to act as the navigation context and navigate to the first page
-                rootFrame = new Frame();
+					// Do not repeat app initialization when the Window already has content,
+					// just ensure that the window is active
+					if (rootFrame == null)
+					{
+						// Create a Frame to act as the navigation context and navigate to the first page
+						rootFrame = new Frame();
 
-                if (args.PreviousExecutionState == ApplicationExecutionState.Terminated)
-                {
-                    //TODO: Load state from previously suspended application
-                }
+						if (args.PreviousExecutionState == ApplicationExecutionState.Terminated)
+						{
+							//TODO: Load state from previously suspended application
+						}
 
-                // Place the frame in the current Window
-                Window.Current.Content = rootFrame;
-            }
+						// Place the frame in the current Window
+						Window.Current.Content = rootFrame;
+					}
 
-            if (rootFrame.Content == null)
-            {
-                // When the navigation stack isn't restored navigate to the first page,
-                // configuring the new page by passing required information as a navigation
-                // parameter
-                if (!rootFrame.Navigate(typeof(MainPage), args.Arguments))
-                {
-                    throw new Exception("Failed to create initial page");
-                }
-            }
-            // Ensure the current window is active
-            Window.Current.Activate();
-        }
+					if (rootFrame.Content == null)
+					{
+						// When the navigation stack isn't restored navigate to the first page,
+						// configuring the new page by passing required information as a navigation
+						// parameter
+
+						var setup = new Setup(rootFrame);
+						setup.Initialize();
+
+						var start = Cirrious.CrossCore.Mvx.Resolve<Cirrious.MvvmCross.ViewModels.IMvxAppStart>();
+						start.Start();
+					}
+					// Ensure the current window is active
+					Window.Current.Activate();
+				}
 
         /// <summary>
         /// Invoked when application execution is being suspended.  Application state is saved

71315067ec13d05222af30b809a4e502e34380fa
Rob Gibbens <RobGibbens@gmail.com>
2013-09-14 22:53:09 -0400

    Step 61

--- MvvmDemo.iOS/AppDelegate.cs
+++ MvvmDemo.iOS/AppDelegate.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using Cirrious.CrossCore;
 using Cirrious.MvvmCross.Touch.Platform;
+using Cirrious.MvvmCross.ViewModels;
 using MonoTouch.Foundation;
 using MonoTouch.UIKit;
 
@@ -13,26 +15,24 @@ namespace MvvmDemo.iOS
 	[Register("AppDelegate")]
 	public partial class AppDelegate : MvxApplicationDelegate
 	{
-		// class-level declarations
-		UIWindow window;
+		UIWindow _window;
 
-		//
-		// This method is invoked when the application has loaded and is ready to run. In this 
-		// method you should instantiate the window, load the UI into it and then make the window
-		// visible.
-		//
-		// You have 17 seconds to return from this method, or iOS will terminate your application.
-		//
+
 		public override bool FinishedLaunching(UIApplication app, NSDictionary options)
 		{
-			// create a new window instance based on the screen size
-			window = new UIWindow(UIScreen.MainScreen.Bounds);
+			_window = new UIWindow(UIScreen.MainScreen.Bounds);
 
-			// If you have defined a view, add it here:
-			// window.RootViewController  = navigationController;
 
-			// make the window visible
-			window.MakeKeyAndVisible();
+			var setup = new Setup(this, _window);
+			setup.Initialize();
+
+
+			var startup = Mvx.Resolve<IMvxAppStart>();
+			startup.Start();
+
+
+			_window.MakeKeyAndVisible();
+
 
 			return true;
 		}

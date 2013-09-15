4c5192d25b41ab7a7d2065b72c0b43ef14bac579
Rob Gibbens <RobGibbens@gmail.com>
2013-09-14 22:18:27 -0400

    Step 29

--- MvvmDemo.WinPhone/Views/UserView.xaml.cs
+++ MvvmDemo.WinPhone/Views/UserView.xaml.cs
@@ -5,12 +5,13 @@ using System.Net;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Navigation;
+using Cirrious.MvvmCross.WindowsPhone.Views;
 using Microsoft.Phone.Controls;
 using Microsoft.Phone.Shell;
 
 namespace MvvmDemo.WinPhone.Views
 {
-	public partial class UserView : PhoneApplicationPage
+	public partial class UserView : MvxPhonePage
 	{
 		public UserView()
 		{

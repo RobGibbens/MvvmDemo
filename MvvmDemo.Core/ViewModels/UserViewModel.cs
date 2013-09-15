27abdd8f7c94686425770723d899e3643306b819
Rob Gibbens <RobGibbens@gmail.com>
2013-09-14 22:06:17 -0400

    Step 18

--- MvvmDemo.Core/ViewModels/UserViewModel.cs
+++ MvvmDemo.Core/ViewModels/UserViewModel.cs
@@ -8,14 +8,16 @@ namespace MvvmDemo.Core.ViewModels
 		public string FirstName
 		{
 			get { return _firstName; }
-			set { _firstName = value; RaisePropertyChanged(() => FirstName); }
+			set { _firstName = value; RaisePropertyChanged(() => FirstName); RaisePropertyChanged(() => FullName); }
 		}
 
 		private string _lastName = "";
 		public string LastName
 		{
 			get { return _lastName; }
-			set { _lastName = value; RaisePropertyChanged(() => LastName); }
+			set { _lastName = value; RaisePropertyChanged(() => LastName); RaisePropertyChanged(() => FullName); }
 		}
+
+		public string FullName { get { return FirstName + " " + LastName; } }
 	}
 }

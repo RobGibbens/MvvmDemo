f00925cc3a28d50095d3b57a138e71f49951e9ac
Rob Gibbens <RobGibbens@gmail.com>
2013-09-17 23:00:11 -0400

    Step 97

--- MvvmDemo.WinPhone/Views/ConferencesView.xaml.cs
+++ MvvmDemo.WinPhone/Views/ConferencesView.xaml.cs
@@ -5,12 +5,13 @@ using System.Net;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Navigation;
+using Cirrious.MvvmCross.WindowsPhone.Views;
 using Microsoft.Phone.Controls;
 using Microsoft.Phone.Shell;
 
 namespace MvvmDemo.WinPhone.Views
 {
-	public partial class ConferencesView : PhoneApplicationPage
+	public partial class ConferencesView : MvxPhonePage
 	{
 		public ConferencesView()
 		{
